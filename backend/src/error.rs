d755664d11d1738e1ccd7166d1e9cc571d63b7d0
Veronika Ivhed <veronika.ivhed@mavatar.com>
2026-09-16 13:25:02 +0200

    Update error handling so it doesn't send database info to client

--- backend/src/error.rs
+++ backend/src/error.rs
@@ -1,4 +1,4 @@
-use poem::{error::ResponseError, http::StatusCode};
+use poem::{Response, error::ResponseError, http::StatusCode};
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
@@ -25,5 +25,20 @@ impl ResponseError for Error {
             Error::AlreadySubmitted => StatusCode::CONFLICT,
             _ => StatusCode::INTERNAL_SERVER_ERROR,
         }
+    }
+
+    fn as_response(&self) -> Response {
+        match self {
+            Error::NotFound | Error::AlreadySubmitted => log::info!("{self}"),
+            _ => log::error!("{self}"),
+        }
+
+        let message = match self {
+            Error::NotFound => self.to_string(),
+            Error::AlreadySubmitted => self.to_string(),
+            _ => "Internal server error".to_string(),
+        };
+
+        Response::builder().status(self.status()).body(message)
     }
 }
